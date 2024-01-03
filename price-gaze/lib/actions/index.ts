4fe0618715d6b10235963fa1300db980493ce5ec
NilrajMayekar <mayekar.n@northeastern.edu>
2024-01-02 19:01:22 -0500

    Added a file for configuring the scraping actions

---
+++ price-gaze/lib/actions/index.ts
@@ -0,0 +1,17 @@
+"use server"
+
+import { scrapeAmazonProduct } from "../scraper";
+
+export async function scrapeAndStoreProduct(productUrl: string){
+    if(!productUrl){
+        return;
+    }
+
+    try{
+        const scrapeProduct = await scrapeAmazonProduct(productUrl);
+        
+    }
+    catch(err: any){
+        throw new Error(`Failed to create/update the produt: ${err.message}`);
+    }
+}
