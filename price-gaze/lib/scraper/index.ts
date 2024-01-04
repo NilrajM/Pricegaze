8f1b8f26a1c4243efabce750e09bbcd7e361e952
NilrajMayekar <mayekar.n@northeastern.edu>
2024-01-04 18:10:13 -0500

    Added scraping for prices, images, out of stock and description fields of the product

--- price-gaze/lib/scraper/index.ts
+++ price-gaze/lib/scraper/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import * as cheerio from 'cheerio';
-import { extractPrice } from "../util";
+import { extractCurrency, extractDescription, extractPrice } from "../util";
 
 export async function scrapeAmazonProduct(url: string){
     if(!url){
@@ -28,6 +28,8 @@ export async function scrapeAmazonProduct(url: string){
 
         //Extract the product title
         const title = $('#productTitle').text().trim();
+
+        //Extract the current price of the product
         const currPrice = extractPrice(
             $('.priceToPay span.a-price-whole'),
             $('.a-price-whole'),
@@ -35,6 +37,7 @@ export async function scrapeAmazonProduct(url: string){
             $('.a-button-selected .a-color-base'),
         );
 
+        //Extract the original price of the product
         const ogPrice = extractPrice(
             $('#priceblock_ourprice'),
             $('.a-price.a-text-price span.a-offscreen'),
@@ -42,10 +45,50 @@ export async function scrapeAmazonProduct(url: string){
             $('.a-price'),
             $('priceblock_dealprice'),
             $('.a-size-base.a-color-price')
+        );
 
-        )
+        //Extrack whether in stock or not
+        const outOfStock = $('availability span').text().trim().toLowerCase() === 'currently unavailable';
+
+        //Extract image of the product
+        const prdImages = $('#imgBlkFront').attr('data-a-dynamic-image') ||
+        $('#landingImage').attr('data-a-dynamic-image') || '{}';
+
+        //Store the image urls keys in an array
+        const imageUrls = Object.keys(JSON.parse(prdImages));
+        
+        //Extract the currency
+        const currency = extractCurrency(
+            $('.a-price-symbol')
+        );
+
+        const discountRate = $('.savingsPercentage').text().replace(/[-%]/g,"");
+
+        const description = extractDescription($);
         // console.log(response.data); For testing purpose
-        console.log({title, currPrice, ogPrice});
+        // console.log({title, currPrice, ogPrice, outOfStock, prdImages,imageUrls, currency, discountRate});
+
+        const data = {
+            url,
+            currency: currency || "$",
+            prdImage: imageUrls[0],
+            title,
+            currPrice: Number(currPrice) || Number(ogPrice),
+            ogPrice: Number(ogPrice) || Number(currPrice),
+            priceHistory: [],
+            discountRate: Number(discountRate),
+            category: 'category',
+            reviewsCount: 100,
+            stars:4.5,
+            isOutOfStock: outOfStock,
+            description,
+            lowesPrice: Number(currPrice) || Number(ogPrice),
+            highestPrice: Number(ogPrice) || Number(currPrice),
+            averagePrice: Number(currPrice) || Number(ogPrice),
+        }
+        // console.log(data)
+
+        return data;
 
     }
     catch(err: any){
